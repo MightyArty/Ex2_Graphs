705d3085d21feaa08fa5b128ab2072ac05d9f760
Artem Shabalin <tomshabalin95@gmail.com>
2021-11-28 08:55:08 +0200

    DWGraph version 3

--- src/Algo/DWGraph.java
+++ src/Algo/DWGraph.java
@@ -24,6 +24,11 @@ public class DWGraph implements DirectedWeightedGraph {
         this.mc = 0;
     }
 
+    /**
+     * returns the Ex2.node_data by the node_id,
+     * @param key - the node_id
+     * @return the Ex2.node_data by the node_id, null if none.
+     */
     @Override
     public NodeData getNode(int key) {
         if(this.graph.containsKey(key))
@@ -31,19 +36,63 @@ public class DWGraph implements DirectedWeightedGraph {
         return null;
     }
 
+    /**
+     * returns the data of the edge (src,dest), null if none.
+     * Note: this method should run in O(1) time.
+     * @param src
+     * @param dest
+     * @return
+     */
     @Override
     public EdgeData getEdge(int src, int dest) {
-        return null;
+        if(!graph.containsKey(src) || !graph.containsKey(dest) || src == dest)
+            throw new RuntimeException("There is no source or destination or the source is equal to destination");
+        else return edges.get(src).get(dest);
     }
 
+    /**
+     * adds a new node to the graph with the given Ex2.node_data.
+     * Note: this method should run in O(1) time.
+     * @param n
+     */
     @Override
     public void addNode(NodeData n) {
-
+        if(n == null || this.graph.containsKey(n.getKey()))
+            return;
+        int key = n.getKey();
+        this.graph.put(key,n);
+        edges.put(key,new HashMap<>());
+        againstDirection.put(key,new HashSet<>());
+        this.mc++;
     }
 
+    /**
+     * Connects an edge with weight w between node src to node dest.
+     * * Note: this method should run in O(1) time.
+     * @param src - the source of the edge.
+     * @param dest - the destination of the edge.
+     * @param w - positive weight representing the cost (aka time, price, etc) between src-->dest.
+     */
     @Override
     public void connect(int src, int dest, double w) {
-
+        if(!this.graph.containsKey(src) || this.graph.containsKey(dest) || src == dest || w < 0)
+            return;
+        HashMap<Integer, EdgeData> srcNode = edges.get(src);
+        EdgeData edge = new EData(src,dest,w);
+        // if the edge exist then will check the weight of the edge
+        if(srcNode.containsKey(dest)){
+            if(srcNode.get(dest).getWeight() == w)
+                return;
+            // updating only the edge
+            srcNode.put(dest,edge);
+        }
+        // there is no edge so will create new one with the given weight
+        else {
+            srcNode.put(dest,edge);
+            againstDirection.get(dest).add(src);
+            edgeSize++;
+        }
+        this.mc++;
     }
 
     @Override
@@ -61,9 +110,21 @@ public class DWGraph implements DirectedWeightedGraph {
         return null;
     }
 
+    /**
+     * Deletes the node (with the given ID) from the graph -
+     * and removes all edges which starts or ends at this node.
+     * This method should run in O(k), V.degree=k, as all the edges should be removed.
+     * @return the data of the removed node (null if none).
+     * @param key
+     */
     @Override
     public NodeData removeNode(int key) {
-        return null;
+        if(!this.graph.containsKey(key))
+            throw new RuntimeException("The graph does not contain this node!");
+        NodeData deleted = this.graph.get(key); // getting the needed node to delete
+        this.graph.remove(key);
+        this.mc++;
+        // NEED TO WORK
     }
 
     @Override
@@ -73,16 +134,16 @@ public class DWGraph implements DirectedWeightedGraph {
 
     @Override
     public int nodeSize() {
-        return 0;
+        return this.nodeSize();
     }
 
     @Override
     public int edgeSize() {
-        return 0;
+        return this.edgeSize;
     }
 
     @Override
     public int getMC() {
-        return 0;
+        return this.mc;
     }
 }
