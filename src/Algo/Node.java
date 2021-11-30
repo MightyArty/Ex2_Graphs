6f3bb4adbb5afcc52de69432973d6e2433d72bff
Artem Shabalin <tomshabalin95@gmail.com>
2021-11-30 16:28:32 +0200

    node improved

--- src/Algo/Node.java
+++ src/Algo/Node.java
@@ -1,5 +1,6 @@
 package Algo;
 
+import java.awt.*;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -16,6 +17,9 @@ public class Node implements NodeData, Serializable {
     private double weight;
     private String info;
     private Location location;
+    private Color current = Color.WHITE;    //setting the color WHITE to start with (assuming the vertex has no friends yet)
+    private HashMap<Integer, EdgeData> fromSRC; //hash map representing the start of path
+    private HashMap<Integer, EdgeData> toDEST;  //hash map representing the end of path
 
     /**
      * Main Constructor
@@ -46,18 +50,8 @@ public class Node implements NodeData, Serializable {
         this.key = key;
         if(location == null) return;
         this.location = location;
-    }
-
-    /**
-     * Empty constructor only for given key
-     * @param key
-     */
-    public Node(int key){
-        this.key = key;
-        this.location = new Location();
-        this.weight = 0;
-        this.tag = 0;
-        this.info = "";
+        this.fromSRC = new HashMap<>();
+        this.toDEST = new HashMap<>();
     }
 
     /**
@@ -82,6 +76,8 @@ public class Node implements NodeData, Serializable {
         this.weight = other.getWeight();
         this.tag = other.getTag();
         this.info = new String(other.getInfo());
+        this.fromSRC = new HashMap<>();
+        this.toDEST = new HashMap<>();
     }
 
     @Override
@@ -122,6 +118,42 @@ public class Node implements NodeData, Serializable {
     @Override
     public void setTag(int t) {
         this.tag = t;
+    }
+
+    /**
+     * adding the src info to the map
+     * @param key
+     * @param edge
+     */
+    public void addFromSRC(int key, EdgeData edge){
+        fromSRC.put(key,edge);
+    }
+
+    /**
+     * adding the dest info to the map
+     * @param key
+     * @param edge
+     */
+    public void addToDEST(int key, EdgeData edge){
+        toDEST.put(key,edge);
+    }
+
+    /**
+     * return the src
+     * @param key
+     * @return
+     */
+    public EdgeData getFromSRC(int key){
+        return fromSRC.get(key);
+    }
+
+    /**
+     * return the dest
+     * @param key
+     * @return
+     */
+    public EdgeData getFromDEST(int key){
+        return toDEST.get(key);
     }
 
     /**
