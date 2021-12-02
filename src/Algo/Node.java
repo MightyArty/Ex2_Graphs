67e557138680e4ffa7ec183382bb41ccbbc1c790
Artem Shabalin <tomshabalin95@gmail.com>
2021-12-03 00:10:00 +0200

    previous version updated

--- src/Algo/Node.java
+++ src/Algo/Node.java
@@ -20,8 +20,8 @@ public class Node implements NodeData, Serializable {
     private String info;
     private Location location;
     private Color current = Color.WHITE;    //setting the color WHITE to start with (assuming the vertex has no friends yet)
-//    private HashMap<Integer, EdgeData> fromSRC; //hash map representing the start of path
-//    private HashMap<Integer, EdgeData> toDEST;  //hash map representing the end of path
+    private HashMap<Integer, EdgeData> fromSRC; //hash map representing the start of path
+    private HashMap<Integer, EdgeData> toDEST;  //hash map representing the end of path
 
     /**
      * Class constructor
@@ -33,6 +33,8 @@ public class Node implements NodeData, Serializable {
         this.weight = node.getWeight();
         this.tag = 0;
         this.info = node.getInfo();
+        this.fromSRC = new HashMap<>();
+        this.toDEST = new HashMap<>();
     }
 
     /**
@@ -45,6 +47,8 @@ public class Node implements NodeData, Serializable {
         this.weight = 0;
         this.info = "";
         this.tag = 0;
+        this.fromSRC = new HashMap<>();
+        this.toDEST = new HashMap<>();
 
         String[] locArr = loc.split(",");
         double x = Double.parseDouble(locArr[0]); // x coordinate
@@ -92,6 +96,70 @@ public class Node implements NodeData, Serializable {
     @Override
     public void setTag(int t) {
         this.tag = t;
+    }
+
+    /**
+     * adding the src info to the map
+     * @param key
+     * @param edge
+     */
+    public void addFromSRC(int key, EdgeData edge){
+        fromSRC.put(key,edge);
+    }
+
+    /**
+     * adding the dest info to the map
+     * @param key
+     * @param edge
+     */
+    public void addToDEST(int key, EdgeData edge){
+        toDEST.put(key,edge);
+    }
+
+    /**
+     * Iterator for fromSRC map
+     * @return
+     */
+    public Iterator<EdgeData> getFromSRCIter(){
+        return fromSRC.values().iterator();
+    }
+
+    /**
+     * Iterator for toDEST map
+     * @return
+     */
+    public Iterator<EdgeData> getToDESTIter(){
+        return toDEST.values().iterator();
+    }
+
+    public static int getKeys() {
+        return keys;
+    }
+
+    public HashMap<Integer, EdgeData> getFromSRC() {
+        return fromSRC;
+    }
+
+    public HashMap<Integer, EdgeData> getToDEST() {
+        return toDEST;
+    }
+
+    /**
+     * Method to remove given key from src map
+     * @param key
+     * @return
+     */
+    public EdgeData removeSRC(int key){
+        return fromSRC.remove(key);
+    }
+
+    /**
+     * Method to remove given key from dest map
+     * @param key
+     * @return
+     */
+    public EdgeData removeDEST(int key){
+        return toDEST.remove(key);
     }
 
     /**
