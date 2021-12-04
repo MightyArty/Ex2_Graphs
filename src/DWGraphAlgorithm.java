9c9227f636f05e2136154171287d4d817b6a16e8
Artem Shabalin <tomshabalin95@gmail.com>
2021-12-04 21:58:35 +0200

    isConnected

--- src/DWGraphAlgorithm.java
+++ src/DWGraphAlgorithm.java
@@ -1,13 +1,16 @@
 import api.DirectedWeightedGraph;
 import api.DirectedWeightedGraphAlgorithms;
+import api.EdgeData;
 import api.NodeData;
 import com.google.gson.*;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
+import java.util.List;
 
 public class DWGraphAlgorithm implements DirectedWeightedGraphAlgorithms {
 
@@ -42,7 +45,32 @@ public class DWGraphAlgorithm implements DirectedWeightedGraphAlgorithms {
 
     @Override
     public boolean isConnected() {
-        return false;
+        if (DFS() > 1)// There is more than one component
+            return false;
+        else {
+            int edgeSize = this.myGraph.edgeSize();
+            int nodeSize = this.myGraph.nodeSize();
+            if (edgeSize == nodeSize * (nodeSize - 1)) return true;
+            DWGraph temp = this.myGraph;
+            Node node = (Node) this.myGraph.getNode(0);
+            //painting the nodes to gray
+            DFSConnect(temp, node);
+            Iterator<Node> i = (Iterator<Node>) temp.getNode(0);
+            //check if all the nodes is GRAY
+            while (i.hasNext()) {
+                //if one of the nodes is not gray, the graph is not connected
+                if (i.next().getCurrent() != Color.GRAY) return false;
+            }
+            //transposing the graph
+            temp = tran();
+            DFSConnect(temp,node);
+            //check if all the nodes is GRAY again
+            i = (Iterator<Node>) temp.getNode(0);
+            while(i.hasNext())
+                //if one of the nodes is not gray, the transpose graph is not connected
+                if (i.next().getCurrent() != Color.GRAY) return false;
+        }
+        return true;
     }
 
     @Override
@@ -125,5 +153,66 @@ public class DWGraphAlgorithm implements DirectedWeightedGraphAlgorithms {
             exception.printStackTrace();
         }
         return flag;
+    }
+
+    public int DFS() {
+        DWGraph temp=this.myGraph;
+        int count = 0;
+        Iterator<NodeData> i = temp.nodeIter();
+        while (i.hasNext()) {
+            Node node = (Node) i;
+            if (node.getCurrent() == Color.WHITE) {
+                count++;
+                DFS_visit(temp, node);
+            }
+        }
+        return count;
+    }
+
+    public void DFS_visit(DWGraph graph, Node n) {
+        n.setCurrent(Color.GRAY);
+        Iterator<EdgeData> i = graph.edgeIter(n.getKey());
+        while (i.hasNext()) {
+            EdgeData edge = i.next();
+            Node v = (Node) graph.getNode(edge.getDest());
+            if (v.getCurrent() == Color.WHITE) {
+                DFS_visit(graph, v);
+            }
+        }
+        n.setCurrent(Color.BLACK);
+    }
+
+    public void DFSConnect(DWGraph g, Node node) {
+        node.setCurrent(Color.GRAY);
+        Iterator<EdgeData> i = g.edgeIter(node.getKey());
+        //running on all the edges
+        while (i.hasNext()) {
+            EdgeData edge = i.next();
+            //check if we didn't pass this node
+            if (node.getCurrent() == Color.WHITE) {
+                Node v = (Node) g.getNode(edge.getDest());
+                DFSConnect(g, v);
+            }
+        }
+    }
+
+    public DWGraph tran() {
+        DWGraph ans = new DWGraph();
+        Iterator<NodeData> i = this.myGraph.nodeIter();
+        //add the all nodes to the ans graph
+        while (i.hasNext()) {
+            ans.addNode(i.next());
+        }
+        i = this.myGraph.nodeIter();
+        //adding the all edges that exist in myGraph
+        while (i.hasNext()) {
+            Node node = (Node) i.next();
+            Iterator<EdgeData> edge = this.myGraph.edgeIter(node.getKey());
+            while (edge.hasNext()) {
+                EData e = (EData) edge.next();
+                ans.connect(e.getDest(), e.getSrc(), e.getWeight());
+            }
+        }
+        return ans;
     }
 }
